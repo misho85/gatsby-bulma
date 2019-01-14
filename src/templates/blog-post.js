1d4e627e80c398099990e21d578ab04642fc6b14
M150 <stevovic@gmail.com>
2019-01-14 17:23:21 +0100

    update blog post style

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -23,7 +23,7 @@ export const BlogPostTemplate = ({
         <div className="container content">
           <div className="columns">
             <div className="column is-10 is-offset-1">
-              <div className="section">
+              <div className="section is-paddingless-horizontal">
                 <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
                   {title}
                 </h1>
